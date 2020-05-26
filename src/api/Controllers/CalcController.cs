25773f3d2f6f27324165dcc07685b5301398a09d
Jason Scott <jsco2t@outlook.com>
2020-05-25 21:28:02 -0700

    WIP: Initial version of API

---
+++ src/api/Controllers/CalcController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StackPilot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        /*[HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }*/
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{firstValue}, {secondValue}")]
+        public int Add(int firstValue, int secondValue)
+        {
+            return firstValue + secondValue;
+        }
+
+        /*// POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
